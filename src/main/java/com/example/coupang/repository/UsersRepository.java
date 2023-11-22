0ff4bd91a155d6e52cbe846ea3366c37c7a1ab87
jm.ryu <jm.ryu@logifocus.co.kr>
2023-11-22 18:23:21 +0900

    Add: 도메인 테스트 추가
    - 테스트 repository, controller 추가

---
+++ src/main/java/com/example/coupang/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.example.coupang.repository;
+
+import com.example.coupang.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
