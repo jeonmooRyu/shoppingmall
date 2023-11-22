3db69ceb68a64f80b89ddae02ec60dc690fbb6ee
jm.ryu <jm.ryu@logifocus.co.kr>
2023-11-22 18:27:51 +0900

    Doc: 스프링 시큐리티 임시제거
    - 기능 개발을 위해 시큐리티 제거

--- src/main/java/com/example/coupang/config/SecurityConfigration.java
+++ src/main/java/com/example/coupang/config/SecurityConfigration.java
@@ -3,15 +3,15 @@ package com.example.coupang.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfigration {
 
 //    @Bean
