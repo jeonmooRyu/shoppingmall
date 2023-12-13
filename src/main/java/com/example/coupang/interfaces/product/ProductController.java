5f3433851633a8d3cd9deb6f9027661f7926e9d7
jeonmoo <rjm0710@naver.com>
2023-12-14 00:29:26 +0900

    Refact: 상품 페이지 개발
    - 상품 페이지 view 개발
    - 서버 상품관련 개발

--- src/main/java/com/example/coupang/interfaces/product/ProductController.java
+++ src/main/java/com/example/coupang/interfaces/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.coupang.interfaces.product;
 
 import com.example.coupang.application.ProductFacade;
+import com.example.coupang.interfaces.orders.OrdersDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,21 +21,14 @@ public class ProductController {
     public @ResponseBody String registProduct(@RequestBody ProductDto.ProductRegistRequest request) {
         var productRegistCommend = productDtoMapper.of(request);
         var result = productFacade.registProduct(productRegistCommend);
-        return request.toString();
+        return result.toString();
     }
 
     @GetMapping("/{productCode}")
     public String gotoProduct(@PathVariable String productCode, Model model) {
         var product = productFacade.getProduct(productCode);
-
         var response = productDtoMapper.toResponse(product);
         model.addAttribute("product", response);
-
-//        var detail = productDtoMapper.toProductDetail(product);
-//        ProductDto.ProductResponses responses = new ProductDto.ProductResponses();
-//        responses.setProductDetails(List.of(detail));
-
-        model.addAttribute("products", List.of(response));
         return "product";
     }
 }
--- src/main/java/com/example/coupang/interfaces/product/ProductDtoMapper.java
+++ src/main/java/com/example/coupang/interfaces/product/ProductDtoMapper.java
@@ -2,9 +2,9 @@ package com.example.coupang.interfaces.product;
 
 import com.example.coupang.domain.product.Product;
 import com.example.coupang.domain.product.ProductCommend;
+import com.example.coupang.interfaces.orders.OrdersDto;
 import org.mapstruct.*;
 
-import java.util.List;
 
 @Mapper(componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
@@ -14,13 +14,5 @@ public interface ProductDtoMapper {
     ProductCommend.ProductRegist of(ProductDto.ProductRegistRequest request);
 
     ProductDto.ProductResponse toResponse(Product product);
-
-//    @Mappings({
-//            @Mapping(source = "productCode", target = "productDetails.productCode"),
-//            @Mapping(source = "productName", target = "productDetails.productName"),
-//            @Mapping(source = "detailImage", target = "productDetails.detailImage"),
-//            @Mapping(source = "price", target = "productDetails.price")
-//    })
-    ProductDto.ProductResponses.ProductDetail toProductDetail(Product product);
 
 }
