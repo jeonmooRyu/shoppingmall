dc96e03ffa2243b15ebfba1b48424c2479374910
jeonmoo <rjm0710@naver.com>
2023-12-15 00:27:34 +0900

    Refactor: 카드사 추가
    - 우리, 하나 카드 추가

--- src/main/java/com/example/coupang/domain/enums/CardCompany.java
+++ src/main/java/com/example/coupang/domain/enums/CardCompany.java
@@ -7,6 +7,8 @@ public enum CardCompany {
 
     HYUNDAI_CARD("현대카드"),
     SAMSUNG_CARD("삼성카드"),
+    WOORI_CARD("우리카드"),
+    HANA_CARD("하나카드"),
     LOTTE_CARD("롯데카드");
 
     private final String description;
