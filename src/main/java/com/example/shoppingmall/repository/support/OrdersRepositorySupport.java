3da3fedeedaf6e482f446a09f9da9221c8aa70f7
jeonmoo <rjm0710@naver.com>
2023-12-19 18:08:49 +0900

    Refactor: 주문 수정
    - Orders Entity에 총 수량 추가
    - OrdersHistory 컨트롤러 추가
    - OrdersDto 수정
    - 주문조회 메소드 수정
       > 주문번호로 조회
       > 유저번호로 조회
    - OrdersRepositorySupport 추가

---
+++ src/main/java/com/example/shoppingmall/repository/support/OrdersRepositorySupport.java
@@ -0,0 +1,31 @@
+package com.example.shoppingmall.repository.support;
+
+import com.example.shoppingmall.domain.orders.Orders;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.example.shoppingmall.domain.orders.QOrders.orders;
+import static com.example.shoppingmall.domain.orders.QOrdersDetail.ordersDetail;
+import static com.example.shoppingmall.domain.payment.QPayment.payment;
+
+@Repository
+@RequiredArgsConstructor
+public class OrdersRepositorySupport {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Orders> findOrdersByUid(String uid) {
+        return jpaQueryFactory.selectFrom(orders)
+                .distinct()
+                .leftJoin(orders.ordersDetails, ordersDetail)
+                .fetchJoin()
+                .leftJoin(orders.payment, payment)
+                .fetchJoin()
+                .where(orders.uid.eq(uid))
+                .orderBy(orders.createdAt.desc())
+                .fetch();
+    }
+}
